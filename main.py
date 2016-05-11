fdd412914606e1a09aafab7c7c77ae300ebd1d02
James Prestwood <jprestwo@jprestwo-xps.amr.corp.intel.com>
2016-05-11 12:19:48 -0700

    More updates
    Signed-off-by: James Prestwood <jprestwo@jprestwo-xps.amr.corp.intel.com>

--- main.py
+++ main.py
@@ -2,55 +2,73 @@ import RPi.GPIO as GPIO
 import time
 import smtplib
 import sys
+import thread
 
-SMTP_SERVER = 'smtp.gmail.com'
-SMTP_PORT = 587
-GMAIL_USERNAME = 'gun.stand.notification@gmail.com'
-GMAIL_PASSWORD = 'gunstand'
-
+# Raspberry Pi button mappings
 BUTTON_1_PIN = 23
 BUTTON_2_PIN = 24
 BUTTON_3_PIN = 22
 BUTTON_GUN_PIN = 27
+BUZZER_PIN = 18
 
-modifier_map = [None]*30
+MAX_GPIO_PIN = max(BUTTON_1_PIN, BUTTON_2_PIN, BUTTON_3_PIN, BUTTON_GUN_PIN)
+
+# Maps Raspi GPIO buttons to code entry keys
+modifier_map = [None]*MAX_GPIO_PIN
 modifier_map[BUTTON_1_PIN] = 1
 modifier_map[BUTTON_2_PIN] = 2
 modifier_map[BUTTON_3_PIN] = 3
 
+# Used to determine the weight of each button press (3 buttons = 100, 4 = 1000 etc)
 modifier = 100
+# Holds the current code being entered
 current_code = 0
+# The number of entry buttons that have been pressed
 current_idx = 1
-correct_code = 111
+# Number of incorrect passcode tries
 incorrect_count = 0
+# State of the gun on the stand, on or off
 gunState = 'on'
+# State of the system, locked or unlocked
 state = 'locked'
-
-cfg_email = 'prestwoj@gmail.com'
-cfg_subject = 'Gun Stand Notification'
-cfg_invalid_entry = 5
-cfg_invalid_entry_message = 'Incorrect passcode has been entered 5 times'
-
+# Will hold the values from the config files passed in
+config = {}
+# This is just to keep the program running forever
 done = False
+# Maps carrier names to the email suffixs for texting via email
+carrier_map = {
+    'AT&T':'txt.att.net'  ,
+    'T-Mobile':'tmomail.net',
+    'Verizon':'vtext.com',
+    'Sprint':'pm.sprint.com',
+    'Sprint PCS':'messaging.sprintpcs.com',
+    'Virgin Mobile':'vmobl.com',
+    'Tracfone':'mmst5.tracfone.com',
+    'Metro PCS':'mymetropcs.com',
+    'Boost Mobile':'myboostmobile.com',
+    'Cricket':'sms.mycricket.com',
+    'Nextel':'messaging.nextel.com',
+    'Alltel':'message.alltel.com',
+    'Ptel':'ptel.com',
+    'Suncom':'tms.suncom.com',
+    'Qwest':'qwestmp.com',
+    'US Cellular':'email.uscc.net'
+}
 
-GPIO.setmode(GPIO.BCM)
+# Turn a phone number and carrier into a usable email address
+def makeCarrierEmail(number, carrier):
+    global carrier_map
+    return str(number) + '@' + carrier_map[carrier]
 
-GPIO.setup(BUTTON_1_PIN, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
-
-GPIO.setup(BUTTON_2_PIN, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
-
-GPIO.setup(BUTTON_3_PIN, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
-
-GPIO.setup(BUTTON_GUN_PIN, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
-
-GPIO.setup(18, GPIO.OUT)
-
+# Read in a config file and return the config dictionary
 def readConfig(file):
-    config = []
+    c = {}
     line = 1
+    # Open the file and read in each line, also separating by '=' into list
     with open(file) as f:
         content = [x.strip('\n') for x in f.readlines()]
 
+    # Loop through the list and parse the config
     for i in content:
         # Skip empty lines
         if len(i) > 1:
@@ -62,64 +80,89 @@ def readConfig(file):
                 print "Error on line " + str(line) + " : '" + str(i) + "'"
                 exit()
             else:
-                config.append(s)
+                # Add each option and value to the dictionary
+                c[s[0]] = s[1]
             line += 1
+    # Return the dictionary
+    return c
 
-    return config
-
+# Send an email notification
 def sendNotification(email, message):
+    global config
+
+    # Setup email values
     recipient = email
-    subject = cfg_subject
+    subject = config['EmailSubject']
     emailText = message
 
     emailText = "" + emailText + ""
 
-    headers = ["From: " + GMAIL_USERNAME,
+    headers = ["From: " + config['EmailUser'],
                "Subject: " + subject,
                "To: " + recipient,
                "MIME-Version: 1.0",
                "Content-Type: text/html"]
     headers = "\r\n".join(headers)
 
-    session = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
+    session = smtplib.SMTP(config['SMTPServer'], int(config['SMTPPort']))
 
     session.ehlo()
     session.starttls()
     session.ehlo
 
-    session.login(GMAIL_USERNAME, GMAIL_PASSWORD)
+    session.login(config['EmailUser'], config['EmailPass'])
 
-    session.sendmail(GMAIL_USERNAME, recipient, headers + "\r\n\r\n" + emailText)
+    session.sendmail(config['EmailUser'], recipient, headers + "\r\n\r\n" + emailText)
     session.quit()
 
+# Play the success tone (3 beeps)
 def playPass():
+    global config
     count = 0
-    while count < 3:
+
+    while count < int(config['BeepsOnSuccess']):
         inner = 0
-        while inner < 20:
-           GPIO.output(18, True)
-           time.sleep(0.001)
-           GPIO.output(18, False)
-           time.sleep(0.001)
-           inner += 1
+        while inner < int(config['SuccessBeepLength']):
+            # Sofware driven PWM
+            GPIO.output(18, True)
+            time.sleep(0.001)
+            GPIO.output(18, False)
+            time.sleep(0.001)
+            inner += 1
 
         time.sleep(0.1)
         count += 1
 
+# Play the error tone (1 long beep)
 def playError():
+    global config
     count = 0
-    while count < 40:
+
+    while count < int(config['ErrorBeepLength']):
+        # Software driven PWM
         GPIO.output(18, True)
         time.sleep(0.01)
         GPIO.output(18, False)
         time.sleep(0.01)
         count += 1
 
+# Should be started in a new thread, plays pass or fail tones
+def playThread(name, fail):
+    if fail:
+        playError()
+    else:
+        playPass()
+
+# Starts a new thread to play the pass/fail tones
+def playTone(fail):
+    thread.start_new_thread(playThread, ("Play-Thread", fail))
+
+# Reset the state, determines if code entry was correct and responds
 def reset():
+    global config
     global current_code
     global modifier
     global current_idx
-    global correct_code
     global incorrect_count
     global cfg_invalid_entry
     global cfg_email
@@ -127,15 +170,19 @@ def reset():
 
     print("Reset!")
     print("Code entered = " + str(current_code))
-    if current_code != correct_code:
-        playError()
+    if current_code != config['Passcode']:
+        playTone(False)
         incorrect_count += 1
-        if int(incorrect_count) >= int(cfg_invalid_entry):
+        if incorrect_count > int(config['InvalidEntryLimit']):
             print "Incorrect code entered 5 times, sending email notification"
-            sendNotification(cfg_email, cfg_invalid_entry_message)
+            if config['TextAlert'] == 'on':
+                email = makeCarrierEmail(config['PhoneNumber'], config['PhoneCarrier'])
+                sendNotification(email, config['InvalidEntryMessage'])
+            if config['EmailAlert'] == 'on':
+                sendNotification(config['NotifierEmail'], config['InvalidEntryMessage'])
             incorrect_count = 0
     else:
-        playPass()
+        playTone(True)
         incorrect_count = 0
         if state == 'unlocked':
             if gunState == 'off':
@@ -150,15 +197,21 @@ def reset():
     modifier = 100
     current_idx = 1
 
+# Callback for gun removal/placement
 def gunTriggered(channel):
     global gunState
     global state
+    global config
 
     if gunState == 'on':
         print "Gun removed"
         if state == 'locked':
-            playError()
-            sendNotification(cfg_email, "The gun was removed without passcode")
+            playTone(False)
+            if config['TextAlert'] == 'on':
+                email = makeCarrierEmail(config['PhoneNumber'], config['PhoneCarrier'])
+                sendNotification(email, config['InvalidRemovalMessage'])
+            if config['EmailAlert'] == 'on':
+                sendNotification(config['NotifierEmail'], config['InvalidRemovalMessage'])
         else:
             print "Gun was removed correctly"
         gunState = 'off'
@@ -166,7 +219,7 @@ def gunTriggered(channel):
         print "Gun placed"
         gunState = 'on'
 
-# Add up the code presses to form the final code entry after 3 presses
+# Callback for the 3 code entry buttons
 def buttonCallback(channel):
     global current_idx
     global current_code
@@ -181,26 +234,25 @@ def buttonCallback(channel):
     if current_idx == 4:
         reset()
 
-config = readConfig(sys.argv[1])
+# Set the GPIO mode
+GPIO.setmode(GPIO.BCM)
 
-for i in config:
-    if i[0] == 'EmailUser':
-        GMAIL_USERNAME = i[1]
-    elif i[0] == 'EmailPass':
-        GMAIL_PASSWORD = i[1]
-    elif i[0] == 'SMTPServer':
-        SMTP_SERVER = i[1]
-    elif i[0] == 'SMTPPort':
-        SMTP_PORT = i[1]
-    elif i[0] == 'NotifierEmail':
-        cfg_email = i[1]
-    elif i[0] == 'EmailSubject':
-        cfg_subject = i[1]
-    elif i[0] == 'InvalidEntryLimit':
-        cfg_invalid_entry = i[1]
-    elif i[0] == 'InvalidEntryMessage':
-        cfg_invalid_entry_message = i[1]
+# Setup all the keypad buttons
+GPIO.setup(BUTTON_1_PIN, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+GPIO.setup(BUTTON_2_PIN, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+GPIO.setup(BUTTON_3_PIN, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
 
+# Setup the Piezo buzzer
+GPIO.setup(BUZZER_PIN, GPIO.OUT)
+
+# Read in the configuration file (passed as the first argument)
+if len(sys.argv) < 2:
+    print "Usage: python main.py <config file>"
+    exit()
+else:
+    config = readConfig(sys.argv[1])
+
+# Add event handlers for the 4 buttons
 GPIO.add_event_detect(BUTTON_1_PIN, GPIO.RISING, callback=buttonCallback, bouncetime=300)
 GPIO.add_event_detect(BUTTON_2_PIN, GPIO.RISING, callback=buttonCallback, bouncetime=300)
 GPIO.add_event_detect(BUTTON_3_PIN, GPIO.RISING, callback=buttonCallback, bouncetime=300)
@@ -209,6 +261,7 @@ GPIO.add_event_detect(BUTTON_GUN_PIN, GPIO.RISING, callback=gunTriggered, bounce
 print "Initial gun state = " + gunState
 print "Initial state = " + state
 
+# Run forever
 while True:
     if done == True:
         break
